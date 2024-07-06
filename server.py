653218ea4fcfaa92174ced80a1dbe457f97d3133
jmorettoni <jmorettoni@gmail.com>
2024-07-06 16:53:22 -0300

    adicionando opcao de servidor web no python

---
+++ server.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__, static_folder='public', static_url_path='')
+
+# Endpoint de exemplo
+@app.route('/api/test')
+def test():
+    return jsonify({'message': 'Servidor Flask está funcionando!'})
+
+# Rota para servir o arquivo HTML principal
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8100)
